ab5b36d8e835e30ae4071aae20a611f420e39946
Mattias <ma10s@hotmail.com>
2019-10-09 18:10:34 +0200

    added the demo

--- demo.py
+++ demo.py
@@ -7,6 +7,11 @@ input_image = pickle.load(open('ultrasound_nerve_segmentation_data/input_image.p
 mask = pickle.load(open('ultrasound_nerve_segmentation_data/mask.p', "rb"))
 
 imput_image = np.expand_dims(input_image,3)
+mask = np.expand_dims(mask,3)
+
+print("imput image shape: ", imput_image.shape)
+print("mask shape: ", mask.shape)
+
 
 model = Unet_model(input_shape=(420,580,1), con_layers=[25, 50, 100])
 
