c58438ea3db783eee9ad840add76329bb7b94dfc
Mattias <ma10s@hotmail.com>
2019-10-09 17:07:30 +0200

    added the demo

---
+++ ultrasound_nerve_segmenetation_data_prep_data.py
@@ -0,0 +1,47 @@
+import numpy as np
+import pickle
+import time
+from PIL import Image
+import os
+from matplotlib import pylab as plt
+
+path = "C:/Users/ma10s/Documents/project/ultrasound-nerve-segmentation/train"
+
+im = []
+im_names = []
+mask = []
+mask_names = []
+i = 0
+for f in os.listdir(path):
+    if i<1000000:
+        f2 = f.split("_")
+        # print("f2: ", f2)
+        if f2[-1] == 'mask.tif':
+            mask.append(Image.open(os.path.join(path, f)))
+            im_name = f2[0] + "_" + f2[1] + ".tif"
+            im.append(Image.open(os.path.join(path, im_name)))
+
+
+    i+=1
+
+im = np.array([np.asarray(image)for image in im])
+mask = np.array(np.array([np.asarray(image)for image in mask],dtype=np.bool_),dtype=np.int32)
+
+print("im shape: ", im.shape)
+print("mask shape: ", mask.shape)
+
+# mask_ind = np.argsort(mask_names)
+# print("mask_ind: ", mask_ind)
+# mask_names = np.asarray(mask_names)[mask_ind]
+#
+# im_ind = np.argsort(im_names)
+# im_names = np.asarray(im_names)[im_ind]
+# print("im_names: ", im_names)
+#
+# for im_n, mask_n in zip(im_names,mask_names):
+#     print(im_n, " - ", mask_n)
+#
+# print("im[0] type", type(im[0]))
+# print("numpy? ", im.shape)
+# print("mask_names: ", mask_names)
+
