0d3fc3e16389e8c53ac7837a4a70ad6e5f4fd2bb
Mattias <ma10s@hotmail.com>
2019-10-09 18:35:48 +0200

    added the demo

--- demo.py
+++ demo.py
@@ -12,8 +12,18 @@ mask = np.expand_dims(mask,3)
 print("input image shape: ", input_image.shape)
 print("mask shape: ", mask.shape)
 
+mask_fraction = np.sum(mask)/mask.size
+
+print("mask fraction: ", mask_fraction)
+
+
+train_input = input_image[:5000]
+train_output = mask[:5000]
+val_input = input_image[5000:]
+val_output = mask[5000:]
+
 
 model = Unet_model(input_shape=(420,580,1), con_layers=[4,8,16])
 
-model.train(input_image,mask,verbose=1,batch_size=8)
+model.train(train_input,train_output,val_input,val_output, verbose=1,batch_size=8)
 
