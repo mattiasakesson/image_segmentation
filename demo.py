62b8b760348fefb717b25d116f8ec74bb5b6b122
Mattias <ma10s@hotmail.com>
2019-10-09 17:49:24 +0200

    added the demo

--- demo.py
+++ demo.py
@@ -1,7 +1,12 @@
 from u_net import Unet_model
 import pickle
+import numpy as np
 
-validation_ts = pickle.load(open('datasets/' + modelname + '/validation_ts.p', "rb" ) )[:,:,species]
 
-model = Unet_model()
+input_image = pickle.load(open('ultrasound_nerve_segmentation_data/input_image.p', "rb"))
+mask = pickle.load(open('ultrasound_nerve_segmentation_data/mask.p', "rb"))
+
+imput_image = np.expand_dims(input_image,3)
+
+model = Unet_model(input_shape=(420,580,1))
 
